f5156ff327c44709dd76fa213b57f349b3ebab95
afatouh7 <70285514+afatouh7@users.noreply.github.com>
2022-02-10 05:06:42 +0200

    initial Commit

---
+++ Api/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Api.Data;
+using Api.Entites;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StorContext _context;
+
+        public ProductsController(StorContext context)
+        {
+            _context = context;
+        } 
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync(); 
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var produt=await _context.Products.FindAsync(id);
+            return Ok(produt);
+        }
+    }
+}
---
+++ Infrastructure/Data/StorContext.cs
@@ -0,0 +1,17 @@
+﻿using Api.Entites;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Api.Data
+{
+    public class StorContext :DbContext
+    {
+        public StorContext(DbContextOptions<StorContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Products>  Products { get; set; }
+
+       
+    }
+}
